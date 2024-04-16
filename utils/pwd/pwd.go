a7e613db7c35d31d7e67aeb9963101cbc7f4c93f
only1970 <xuweiyi0329@foxmail.com>
2024-04-16 09:06:44 +0800

    update  加解密函数

---
+++ utils/pwd/pwd.go
@@ -0,0 +1,28 @@
+package pwd
+
+import (
+	"gvd_server/global"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+// HashPwd hash密码
+func HashPwd(pwd string) string {
+	hash, err := bcrypt.GenerateFromPassword([]byte(pwd), bcrypt.MinCost)
+	if err != nil {
+		global.Log.Errorln(err)
+	}
+	return string(hash)
+}
+
+// CheckPwd 验证密码  hash之后的密码  输入的密码
+func CheckPwd(hashPwd string, pwd string) bool {
+	byteHash := []byte(hashPwd)
+
+	err := bcrypt.CompareHashAndPassword(byteHash, []byte(pwd))
+	if err != nil {
+		global.Log.Errorln(err)
+		return false
+	}
+	return true
+}
